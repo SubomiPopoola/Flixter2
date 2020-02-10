735ddd152517ec4dd64037cad65e4af179f0dbd1
SubomiPopoola <60233501+SubomiPopoola@users.noreply.github.com>
2020-02-09 22:31:34 -0500

    Add files via upload

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Subomi Popoola on 2/9/20.
+//  Copyright © 2020 Subomi Popoola. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
